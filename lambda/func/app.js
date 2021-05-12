463e3492d07f1cac02599020b607d4867564077f
Matthew Kosloski <matthewkosloski@me.com>
2021-05-12 05:35:43 -0500

    updated lambda code

--- lambda/func/app.js
+++ lambda/func/app.js
@@ -9,7 +9,7 @@ module.exports.handler = async (event) => {
 			semanticVersion: '3.0.0'
 		}
 	};
-	
+
 	if (event != undefined) {
 		// An event object has been provided,
 		// so merge the default properties with
@@ -42,31 +42,73 @@ module.exports.handler = async (event) => {
 		};
 	}
 
-	const programPath = './a.out';
+	// The tmp directory given to the lambda function to
+	// be used as an empheral storage location. The user
+	// running within the lambda's container has rwx permissions
+	// in this directory.
+	const tmpDir = `../tmp`;
 
-	const cdCmd = `cd ./${semanticVersion}`;
-	const echoCmd = `echo "${program}"`;
-	const javaCmd = `java -jar torreyc-${semanticVersion}.jar`;
-	const programCmd = `[ -f ${programPath} ] && ${programPath} && rm ${programPath}`;
+	// The location to which the Torrey compilers have been downloaded.
+	const compilersRootDir = `../tmp2/compilers`;
 
-	// Change directory to the compiler version's folder and pipe in the program
-	// to the compiler as standard input.
-	let command = `${cdCmd} && ${echoCmd} | ${javaCmd}`;
-	
+	// The directory to the selected compiler.
+	const compilerDir = `${compilersRootDir}/${semanticVersion}`;
+
+	// The path to the selected compiler's jar file.
+	const torreycPath = `${compilerDir}/torreyc-${semanticVersion}.jar`;
+
+	// The path to the selected compiler's runtime.
+	const runtimePath = `${compilerDir}/runtime.c`;
+
+	// The path to which the temporary assembly program will be written.
+	const asmPath = `${tmpDir}/temp.s`;
+
+	// The path at which the executable will live.
+	const execPath = `${tmpDir}/a.out`;
+
+	// The path to which the runtime's object code will be written.
+	const objCodePath = `${tmpDir}/runtime.o`;
+
+	let cmd = ``;
+
+	// Run the compiler with the input program.
+	cmd = `${cmd} echo "${program}" | java -jar ${torreycPath}`;
+
+	// If compiler flags are provided, then run
+	// the compiler with them.
 	if (flags.length != 0)
-		command = `${command} ${flags.join(' ')}`;
-		
+		cmd = `${cmd} ${flags.join(' ')}`;
+
 	if (!(flags.includes('-L') || flags.includes('-p') || flags.includes('-ir')
 		|| flags.includes('-S'))) {
-		// Neither -L, -p, -ir, or -S have been provided,
-		// so run the executable and subsequently delete it.
-		command = `${command} && ${programCmd}`;
+		// No "breakpoint" flag has been provided, so build
+		// an executable and run it.
+
+		// Write the assembly file to /tmp.
+		cmd = `${cmd} -S > ${asmPath}`;
+
+		// Build the runtime object code and save it to /tmp.
+		cmd = `${cmd} && gcc -c ${runtimePath}`;
+		cmd = `${cmd} -o ${objCodePath}`;
+
+		// Assemble and link the runtime with the assembly
+		// to build an executable.
+		cmd = `${cmd} && gcc ${asmPath} ${objCodePath} -o ${execPath}`;
+
+		// Run the program.
+		cmd = `${cmd} && ${execPath}`;
+
+		// Delete the temp assembly file and
+		// the produced executable file.
+		cmd = `${cmd} && rm ${asmPath} && rm ${execPath}`;
 	}
 
-	const { stdout, stderr } = await exec(command);
+	// Execute the above constructed bash command and return
+	// the contents of the standard output and error streams.
+	const { stdout, stderr } = await exec(cmd);
 
 	return {
 		statusCode: 200,
-		message: {stdout, stderr}
+		message: { stdout, stderr }
 	};
 };
