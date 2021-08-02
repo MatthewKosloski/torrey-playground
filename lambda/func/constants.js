4ae4b2d1a897cc0021e9a5c024701d10d42901fa
Matthew Kosloski <matthewkosloski@me.com>
2021-08-02 00:07:55 -0500

    converted to 2 tabs

--- lambda/func/constants.js
+++ lambda/func/constants.js
@@ -1,6 +1,6 @@
 // Constants used by the Lambda handler.
 module.exports = {
-    MAX_PROGRAM_LENGTH: 500,
-    CONFIG_FILE_PATH: './config.json',
-    CONFIG_ENCODING: 'utf8'
+	MAX_PROGRAM_LENGTH: 500,
+	CONFIG_FILE_PATH: './config.json',
+	CONFIG_ENCODING: 'utf8'
 };
