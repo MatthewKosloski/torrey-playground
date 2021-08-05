cded7f69dcd092dbf032c363b7614005002e0656
Matthew Kosloski <matthewkosloski@me.com>
2021-08-04 20:24:01 -0500

    increase character limit to 1024

--- lambda/func/constants.js
+++ lambda/func/constants.js
@@ -1,6 +1,6 @@
 // Constants used by the Lambda handler.
 module.exports = {
-	MAX_PROGRAM_LENGTH: 500,
+	MAX_PROGRAM_LENGTH: 1024,
 	CONFIG_FILE_PATH: './config.json',
 	CONFIG_ENCODING: 'utf8'
 };
