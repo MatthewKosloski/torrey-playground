615f2c6ab1557bd62ebe25301a93fd280a0f3823
Matthew Kosloski <matthewkosloski@me.com>
2021-09-11 05:33:02 -0500

    test new services

--- lambda/func/src/LambdaHandler.ts
+++ lambda/func/src/LambdaHandler.ts
@@ -1,21 +1,33 @@
 import { Service } from 'typedi';
-import { LoggingService, ResponseService, ResponseObject } from './services';
 import { Event } from './app';
+import { SuccessfulOperation } from './OperationResult';
+import { LoggingService, ResponseService, RunService } from './services';
+import { ExecutionResult } from './services/RunService/RunService';
+import { ResponseObject } from './services/ResponseService';
 
 @Service()
 export class LambdaHandler {
 	constructor(
 		private _loggingService: LoggingService,
-		private _responseService: ResponseService
+		private _responseService: ResponseService,
+		private _runService: RunService
 	) {}
 
-	public handle(event: Event): ResponseObject {
-		if (event.program === 'OK')
-			return this._responseService.ok('OK Response test :)');
-		else if (event.program === 'Bad Request')
-			return this._responseService.badRequest('Bad Response test :)');
-		else if (event.program === 'Internal Server Error')
-			return this._responseService.internalServerError('Internal Server Error test :)');
+	public async handle(event: Event): Promise<ResponseObject> {
+		const runResult = await this._runService.run(
+			'(println (+ 32 10))',
+			[],
+			'3.0.3',
+			'../run.sh',
+			'../../tmp',
+			'../../tmp2/compilers'
+		);
+
+		if (runResult instanceof SuccessfulOperation) {
+			return this._responseService.ok(runResult.getResult() as ExecutionResult);
+		} else {
+			return this._responseService.internalServerError(runResult.getResult() as string);
+		}
 	}
 
 }
