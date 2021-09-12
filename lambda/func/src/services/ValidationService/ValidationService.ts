2fedbd2ee970fbdfc6f99ca566e84d36cde3b743
Matthew Kosloski <matthewkosloski@me.com>
2021-09-12 00:41:26 -0500

    implemented OperationResult in ValidationService

--- lambda/func/src/services/ValidationService/ValidationService.ts
+++ lambda/func/src/services/ValidationService/ValidationService.ts
@@ -1,24 +1,29 @@
 import { Service } from 'typedi';
 import { validate } from 'jsonschema';
 import { configSchema, eventSchema } from './schemas';
+import { SuccessfulOperation, FailedOperation, OperationResult } from '../../OperationResult';
+import { Event } from '../../app';
 
 @Service()
 export class ValidationService {
 
-	public validateEvent(event: Event): string[] {
+	public validateEvent(event: Event): OperationResult<null | string[]> {
 		return this._validate(event, eventSchema);
 	}
 
-	public validateConfig(config: any): string[] {
+	public validateConfig(config: any): OperationResult<null | string[]> {
 		return this._validate(config, configSchema);
 	}
 
-	private _validate(instance: any, schema: any): string[] {
+	private _validate(instance: any, schema: any): OperationResult<null | string[]> {
 		const { errors } = validate(instance, schema);
+
+		if (errors.length === 0)
+			return new SuccessfulOperation();
 
 		// Convert errors from an array of objects to
 		// an array of strings.
-		const result = errors.map(({path, message}) => {
+		const errorStrings = errors.map(({path, message}) => {
 			const reducedPath = path.reduce((a, b) => {
 				if (typeof b === 'number') {
 					return `${a}[${b}]`;
@@ -31,6 +36,6 @@ export class ValidationService {
 			return `${reducedPath} ${message}`;
 		});
 	
-		return result;
+		return new FailedOperation(errorStrings);
 	}
 }
